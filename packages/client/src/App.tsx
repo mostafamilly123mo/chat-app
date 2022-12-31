6412038593c7a403542d2c42ca068d5e1014337b
Mustafa Kashoul Melli <mostafamilly6@gmail.com>
2022-12-31 20:23:39 +0300

    Trying socket broadcast

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,30 @@
-function App() {
-  return <div>Chat app</div>;
-}
+import React from "react";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:8000");
 
+const App = () => {
+  const [time, setTime] = React.useState("fetching");
+  React.useEffect(() => {
+    socket.on("connect", () => console.log(socket.id));
+    socket.on("receive", (data) => {
+      console.log(data);
+    });
+    socket.on("connect_error", () => {
+      setTimeout(() => socket.connect(), 5000);
+    });
+    socket.on("time", (data) => setTime(data));
+    socket.on("disconnect", () => setTime("server disconnected"));
+  }, []);
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          socket.emit("receive", "Hola");
+        }}
+      >
+        Say Hii
+      </button>
+    </div>
+  );
+};
 export default App;
--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,15 +1,40 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+const http = require("http").Server(app);
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+app.use(cors());
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+socketIO.on("connection", (socket: any) => {
+  console.log("client connected: ", socket.id);
+  socket.emit("chat message", "Hiiii");
+  socket.join("clock-room");
+
+  socket.on("receive", (data: any) => {
+    socket.broadcast.emit("receive", `Received ${data}`);
+  });
+
+  socket.on("disconnect", (reason: any) => {
+    console.log(reason);
+  });
+});
+setInterval(() => {
+  socketIO.to("clock-room").emit("time", new Date());
+}, 1000);
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+http.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
