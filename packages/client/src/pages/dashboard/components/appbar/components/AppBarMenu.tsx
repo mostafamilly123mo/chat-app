bae57b1b5d81dad2ebdd1a88cb4081480a20b6c8
Mustafa Kashoul Melli <mostafamilly6@gmail.com>
2023-01-01 09:28:04 +0200

    Add logout icon

--- packages/client/src/pages/dashboard/components/appbar/components/AppBarMenu.tsx
+++ packages/client/src/pages/dashboard/components/appbar/components/AppBarMenu.tsx
@@ -3,6 +3,9 @@ import { IconButton, Menu, MenuItem } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 export const AppBarMenu = () => {
   const navigate = useNavigate();
@@ -37,7 +40,12 @@ export const AppBarMenu = () => {
         )}
       </IconButton>
       <Menu anchorEl={anchorEl} open={open} onClose={handleClose} elevation={1}>
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        <MenuItem onClick={handleLogout}>
+          <ListItemIcon>
+            <LogoutIcon fontSize="small" sx={{ color: "common.black" }} />
+          </ListItemIcon>
+          <ListItemText>Logout</ListItemText>
+        </MenuItem>
       </Menu>
     </>
   );
